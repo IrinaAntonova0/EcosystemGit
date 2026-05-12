27325188f5ec7e0d52603b7437fdab11243f1c73
IrinaAntonova0 <irina.antonova.dipl.inf@gmail.com>
2026-05-12 14:54:50 +0200

    Initial Main 2 method-stubs todo

---
+++ src/main/java/MainBranch.java
@@ -0,0 +1,13 @@
+public class MainBranch {
+    static void main(String[] args) {
+        System.out.println("MainBranch.main");
+    }
+
+    public static void printA(){
+        //todo
+    }
+
+    public static void printB(){
+        //todo
+    }
+}
